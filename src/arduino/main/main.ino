1070bbab176c4bf3b657bd7d196815ed3e18e39b
Agrofx <58386168+agrofx1@users.noreply.github.com>
2024-05-15 21:44:11 +0300

    Http server

--- src/arduino/main/main.ino
+++ src/arduino/main/main.ino
@@ -7,6 +7,8 @@
 char ssid[16] = "";
 char password[16] = "";
 
+AsyncWebServer server(80);
+
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
@@ -24,21 +26,23 @@ void TimeSync(void) {
   timeClient.begin();
   timeClient.setTimeOffset(10800);
 }
+void ServerSetup(void) {
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", "<p>hello!</p>");
+  });
+  server.begin();
+}
 void setup() {
   Serial.begin(9600);
   WiFiConnect();
   TimeSync();
   lcd.init();
   lcd.backlight();
+  ServerSetup();
 }
 void loop() {
   timeClient.update();
-  lcd.setCursor(0, 0);
-  lcd.print(String(timeClient.getHours())+":"+String(timeClient.getMinutes()));
-  delay(1000);
-  lcd.clear();
-  lcd.setCursor(0,1);
-  lcd.print(String(timeClient.getHours())+":"+String(timeClient.getMinutes()));
-  delay(1000);
   lcd.clear(); 
+  lcd.setCursor(0, 8);
+  lcd.print(String(timeClient.getHours())+":"+String(timeClient.getMinutes()));
 }
