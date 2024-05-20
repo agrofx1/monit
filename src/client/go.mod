f737f344f576e900190832533906ec15dfbf1c19
Agrofx <58386168+agrofx1@users.noreply.github.com>
2024-05-20 20:41:52 +0300

    Client (Go)

---
+++ src/client/go.mod
@@ -0,0 +1,12 @@
+module monitclient
+
+go 1.22.3
+
+require (
+	github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/ssimunic/gosensors v0.0.0-20170414000417-e7ab9a4e799b // indirect
+	github.com/tklauser/go-sysconf v0.3.14 // indirect
+	github.com/tklauser/numcpus v0.8.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+)
---
+++ src/client/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"time"
+
+	"net/http"
+
+	"github.com/pbnjay/memory"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/host"
+	"github.com/ssimunic/gosensors"
+)
+
+var URI string = os.Args[1];
+var CODE string = os.Args[2];
+
+var CHIP string = os.Args[3];
+var VAL string = os.Args[4];
+
+func main() {
+	fmt.Println("Starting...");
+	base64.RawStdEncoding.WithPadding(base64.NoPadding)
+
+	for range time.Tick(time.Second * 2) {
+		bytes := []byte(getStats())
+		var base string = base64.RawURLEncoding.EncodeToString(bytes);
+		sendData(base);
+	}
+}
+
+func getStats() string {
+	var result string;
+	percent,cpu_err := cpu.Percent(time.Second,false);
+	system,system_err := host.Info();
+	sensors,sensors_err := gosensors.NewFromSystem()
+	ram := int(memory.TotalMemory() / 1000000000);
+	ram_free := int(memory.FreeMemory() / 1000000000);
+	ram_usage := int(ram - ram_free);
+	hostname := system.Hostname;
+	var temp string;
+	if (cpu_err != nil || system_err != nil || sensors_err != nil) {
+		return "Error"
+	}
+	for chip := range sensors.Chips {
+		if (chip == CHIP) {
+			for key, value := range sensors.Chips[chip] {
+				if key == VAL {
+					temp = value;
+				}
+			}
+		}
+
+	}
+	if (temp == "") {
+		temp = " "
+	} else {
+		temp = "/"+temp[1:3]+"C "
+	}
+	
+	
+	result = hostname+" "+strconv.Itoa(int(math.Ceil(percent[0])))+"%"+temp+strconv.Itoa(ram_usage)+"/"+strconv.Itoa(ram)+"G";
+	return result
+}
+
+func sendData(data string) {
+	fmt.Println(URI+fmt.Sprintf("?code=%s&str=%s", CODE, data))
+	http.Get(URI+fmt.Sprintf("?code=%s&str=%s", CODE, data))
+}
